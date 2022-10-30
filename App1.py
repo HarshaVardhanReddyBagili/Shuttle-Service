b90ff698f7ab224a315a4e1a07fe5fc5e046aa42
Harsha Vardhan Reddy Bagili <110410067+HarshaVardhanReddyBagili@users.noreply.github.com>
2022-10-30 13:24:31 +0530

    Update App1.py

--- App1.py
+++ App1.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import networkx.algorithms.community as nx_comm
-#from community import community_louvain
+from community import community_louvain
+import openpyxl
 
-st.title('Optimal traffic routes prediction for shuttle service')
+st.title('Shuttle services - Optimal traffic routes prediction')
 st.markdown('Karate Club Graph')
 
 uploaded_file = st.file_uploader(" ", type=['xlsx']) #Only accepts excel file format
@@ -16,15 +17,12 @@ if uploaded_file is not None:
     data = pd.read_excel(uploaded_file)
     data
     g = nx.karate_club_graph()
-    g = nx.from_pandas_edgelist(data, 'Origin', 'Destination', edge_attr=['Distance_meters','Duration_min_minutes']) #Use the Graph API to create an empty network graph object
+    g = nx.from_pandas_edgelist(data, 'Origin', 'Destination', edge_attr=['Route', 'Distance_meters','Duration_min_minutes'] #Use the Graph API to create an empty network graph object
 
     partition = nx_comm.louvain_communities(g)
 
-    if st.button("Click to show partition: "):
-        st.write(partition, len(partition))
-
     color_map = []
-    # color the nodes according to their partition
+    # color the nodes in partition
     for node in g:
         if node in partition[0]:
             color_map.append('red')
@@ -40,31 +38,25 @@ if uploaded_file is not None:
     pos = nx.spring_layout(g)
 
 
-    nx.draw_networkx(g, pos, partition, 
-                    with_labels=True, 
-                    node_size = 250, 
-                    node_shape = "s", 
-                    edge_color = "k", 
-                    style = "--", 
-                    node_color = color_map,
-                    font_size = 15)
+    nx.draw_networkx(g, pos, partition, with_labels=True, node_size = 250, node_shape = "s", edge_color = "k", style = "--", node_color = color_map, font_size = 15)
     plt.title('Louvain_communities algorithm', fontdict={'fontsize': 40})
 
     st.pyplot(fig)
+
+    if st.button("Click here for Partition: "):
+        st.write(partition, len(partition))
    
-    #########################################
+    ###################################
     st.info("Louvain_community Partition Graph")
 
-    com_4 = nx_comm.louvain_communities(g)
+    com = nx_comm.louvain_communities(g)
 
     st.subheader("For louvain_communities")
-    st.write("Modularity: ", nx_comm.modularity(g, com_4))
-    st.write("Partition Quality: ", nx_comm.partition_quality(g, com_4))
-    st.write("Coverage: ", nx_comm.coverage(g, com_4)) 
-    st.write("Performance: ", nx_comm.performance(g, com_4))
+    st.write("Modularity: ", nx_comm.modularity(g, com))
+    st.write("Partition Quality: ", nx_comm.partition_quality(g, com))
+    st.write("Coverage: ", nx_comm.coverage(g, com)) 
+    st.write("Performance: ", nx_comm.performance(g, com))
 
     if st.button("Click to show edge betweeness centrality of graph"):
         edge_BC = nx.edge_betweenness_centrality(g)
         st.info(sorted(edge_BC.items(), key=lambda edge_BC : (edge_BC[1], edge_BC[0]), reverse = True))
-    
-   
