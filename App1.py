9ba81db9103aab9b2a7f733ef35256d2a6ebe760
Harsha Vardhan Reddy Bagili <110410067+HarshaVardhanReddyBagili@users.noreply.github.com>
2022-10-30 13:54:41 +0530

    Update App1.py

--- App1.py
+++ App1.py
@@ -7,7 +7,7 @@ import numpy as np
 import networkx.algorithms.community as nx_comm
 #from community import community_louvain
 
-st.title('Shuttle services - Optimal traffic routes prediction')
+st.title('Shuttle Services - Optimal traffic routes prediction')
 st.markdown('Karate Club Graph')
 
 uploaded_file = st.file_uploader(" ", type=['xlsx']) #Only accepts excel file format
@@ -16,11 +16,12 @@ if uploaded_file is not None:
     data = pd.read_excel(uploaded_file)
     data
     g = nx.karate_club_graph()
-    g = nx.from_pandas_edgelist(data, source = "Origin", target = "Destination", edge_attr=['Distance_meters','Duration_minutes','Duration_min_minutes','Duration_max_minutes'])#Use the Graph API to create an empty network graph object
+    g = nx.from_pandas_edgelist(data, source = "Origin", target = "Destination", edge_attr=['Distance_meters','Route','Duration_min_minutes']) #Use the Graph API to create an empty network graph object
 
     partition = nx_comm.louvain_communities(g)
-    if st.button("Click to Partition: "):
-         st.write(partition, len(partition))
+
+    if st.button("Click here for Partition: "):
+        st.write(partition, len(partition))
 
     color_map = []
     # color the nodes according to their partition
@@ -47,12 +48,11 @@ if uploaded_file is not None:
                     style = "--", 
                     node_color = color_map,
                     font_size = 15)
+    plt.title('Louvain_communities algorithm', fontdict={'fontsize': 40})
     st.pyplot(fig)
-    plt.title('Louvain_communities algorithm', fontdict={'fontsize': 40}
-  
-
+   
     #########################################
-    st.info("Louvain_community Partition Graph")
+    st.info("louvain_community Partition Graph")
 
     com = nx_comm.louvain_communities(g)
 
@@ -65,3 +65,4 @@ if uploaded_file is not None:
     if st.button("Click to show edge betweeness centrality of graph"):
         edge_BC = nx.edge_betweenness_centrality(g)
         st.info(sorted(edge_BC.items(), key=lambda edge_BC : (edge_BC[1], edge_BC[0]), reverse = True))
+   
